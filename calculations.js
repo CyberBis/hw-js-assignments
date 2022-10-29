de70a85efe83b81d4e09b667227fb74882306e4a
CyberBis <tefka10@gmail.com>
2022-10-29 14:54:57 +0300

    Try change

--- calculations.js
+++ calculations.js
@@ -3,15 +3,16 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(x, y) {
+  x = x - y;
+  return x;
+  //добавляем данные, для того чтобы наша функция работала корректно
 }
 
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
+  // убираем лишний код console.log
 }
 
 function getRestAfterDivision(x, y) {
@@ -21,5 +22,5 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2)); // убираем лишнюю информацию, т.к. у нас идет речь о 2-х операндах
 console.log(getRestAfterDivision(7000, 3));
