7f7315aa36e7bbdd81ce31da730baa92d81bd63d
CyberBis <tefka10@gmail.com>
2022-10-29 16:53:36 +0300

    Added function inc/dec

--- calculations.js
+++ calculations.js
@@ -24,3 +24,18 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2)); // убираем лишнюю информацию, т.к. у нас идет речь о 2-х операндах
 console.log(getRestAfterDivision(7000, 3));
+
+function testIncrement(x, y) {
+  ++y;
+  x += y; // либо мы можем записать все в одной строке x += ++y
+  return x;
+}
+
+function testDecrement(x, y) {
+  --y;
+  x -= y; // либо мы можем записать все в одной строке x += --y
+  return x;
+}
+
+console.log(testIncrement(98, 1));
+console.log(testDecrement(100, 1));
